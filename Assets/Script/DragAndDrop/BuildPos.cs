2d34082d71a4e339ba6b1079994fde2a78139c7c
vinhLong264 <164327473+vinhlong264@users.noreply.github.com>
2024-07-19 22:53:32 +0700

    Tính năng kéo thả Plant
    các thư mục test là asset tôi thêm vào để test kéo thả

---
+++ Assets/Script/DragAndDrop/BuildPos.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class BuildPos : MonoBehaviour,IDropHandler,IPointerEnterHandler,IPointerExitHandler
+{
+    private Image ImagePos;
+    [SerializeField] private GameObject Pos_Obj; // Plant được sinh ra ở vị trí đặt
+
+    void Start()
+    {
+        ImagePos = GetComponent<Image>();
+    }
+
+    public void OnDrop(PointerEventData eventData) // Hàm Drop được lấy từ IDropHandler, có chức năng thả đến 1 vị trí được xác định
+    {
+        if(Pos_Obj != null) // Kiểm tra vị trí hiện tại có cây được đặt chưa
+        {
+            Debug.Log("Vị trí này đã có cây");
+        }
+        else
+        {
+            if(eventData.pointerDrag != null) // kiểu tra xem eventData.pointerDrag có được tham chiếu đến đối tượng được kéo không
+            {
+                Pos_Obj = Instantiate(DragAndDropOBJ.currentObj , transform.position , Quaternion.identity);
+            }
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData) // Hàm có chức năng là trỏ chuột vào đối tượng
+    {
+        ImagePos.color = new Color(1, 0, 0, 0.5f);
+        //Các tham số: red , green , blue , alpha
+    }
+
+    public void OnPointerExit(PointerEventData eventData) // Hàm có chức năng là rời việc trỏ chuột vào đối tượng
+    {
+        ImagePos.color = new Color(1, 0, 0, 0);
+    }
+}
---
+++ Assets/Script/DragAndDrop/PlantSlot.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class PlantSlot : MonoBehaviour, IBeginDragHandler, IDragHandler , IEndDragHandler
+{
+    [Header("Drag")]
+    [SerializeField] private Image Object_Card; // Image sẽ hiện khi kéo
+    [SerializeField] private Canvas canvas;
+    [SerializeField] private CanvasGroup canvasGroup;
+    private Vector3 pos; // Lưu vị trí neo của thẻ
+
+    [Header("Plant Object")]
+    [SerializeField] private GameObject Object_Plant;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("Start on Drag");
+        Object_Card.gameObject.SetActive(true);
+        pos = Object_Card.rectTransform.anchoredPosition; // lấy vị trí neo ban đầu của thẻ gán cho pos, anchoredPosition: Vị trí của rectTransform so với điểm tham chiếu neo
+        canvasGroup.blocksRaycasts = false;
+        canvasGroup.alpha = 0.6f;
+        DragAndDropOBJ.currentObj = Object_Plant;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log("Drag");
+        Object_Card.rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor; // Tính toán việc kéo object
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("Kết thúc kéo thả");
+        Object_Card.gameObject.SetActive(false);
+        Object_Card.rectTransform.anchoredPosition = pos; // Khi kết thúc việc kéo, gắn vị trí điểm neo hiện tại cho pos
+        canvasGroup.blocksRaycasts = true;
+    }
+}
