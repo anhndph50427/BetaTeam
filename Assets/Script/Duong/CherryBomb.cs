baf8a4177a4e114edeab67e5beb897e75ee7a1a0
pesduo <pesduo05@gmail.com>
2024-07-22 10:29:27 +0700

    Update Animation, potatomine, cherrybomb

---
+++ Assets/Script/Duong/CherryBomb.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CherryBomb : MonoBehaviour
+{
+    public Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.CompareTag("Zombie"))
+        {
+            animator.SetTrigger("ATK");
+            Destroy(gameObject, 1f);
+        }
+    }
+}
---
+++ Assets/Script/Duong/PotatoMine.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PotatoMine : MonoBehaviour
+{
+    public Animator animator;
+    public float sproutTime = 5f;
+    private bool isSprouted = false;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        Invoke("Sprout", sproutTime); // Gọi hàm Sprout sau 5 giây
+    }
+
+    void Sprout()
+    {
+        isSprouted = true;
+        animator.SetBool("TroiLen", true);
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.CompareTag("Zombie") && isSprouted)
+        {
+            animator.SetTrigger("ATK");
+            Destroy(gameObject, 1f);
+        }
+    }
+}
