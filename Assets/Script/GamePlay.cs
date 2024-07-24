2674a3d57bff68fe4877037722aa5a1c26c5c3c4
vinhLong264 <164327473+vinhlong264@users.noreply.github.com>
2024-07-24 10:42:04 +0700

    Update game play

---
+++ Assets/Script/GamePlay.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class GamePlay : MonoBehaviour
+{
+    public static GamePlay instance {  get; private set; }
+    public LayerMask whatIsMask;
+    public int sunScore;
+    public Transform target;
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        RaycastHit2D touch = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero, Mathf.Infinity, whatIsMask);
+        if (touch.collider != null)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                touch.collider.gameObject.GetComponent<Sun>().isTouch = true;
+            }
+            
+        }
+    }
+}
