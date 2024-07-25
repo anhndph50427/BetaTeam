a14c752feabba7afc2e0f0d8ea6bbd182406eba6
vinhLong264 <164327473+vinhlong264@users.noreply.github.com>
2024-07-26 00:41:59 +0700

    Update

--- Assets/Script/LoginAndRegister/HighScore.cs
+++ Assets/Script/LoginAndRegister/HighScore.cs
@@ -1,27 +1,68 @@
-using System.Collections;
 using System.Collections.Generic;
-using TMPro;
+using System.Linq;
 using UnityEngine;
+
 
 public class HighScore : MonoBehaviour
 {
-    public List<HighScoreContent> contents = new List<HighScoreContent>();
+    public List<HighScoreContent> HightScoreData = new List<HighScoreContent>();
+    public GameObject ScoreInfo;
+    public Transform Column;
+    public int timeFinish;
     void Start()
     {
-        
-    }
+        HighScoreContent content = new HighScoreContent();
+        content.name = "Latina";
+        content.time = timeFinish;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        foreach (Transform t in Column)
+        {
+            Destroy(t.gameObject);
+        }
+
+        if (HightScoreData.Count < 3)
+        {
+            Debug.Log("Thêm m?t dòng m?i");
+            HightScoreData.Add(content);
+
+            HightScoreData = HightScoreData.OrderBy(x => x.time).ToList();
+
+            for (int i = 0; i < HightScoreData.Count; i++)
+            {
+                HightScoreData[i].rank = i + 1;
+                GameObject lines = Instantiate(ScoreInfo, Column);
+                lines.GetComponent<ScoreLine>().setText(HightScoreData[i]);
+            }
+
+        }
+        else
+        {
+            var min = HightScoreData.OrderByDescending(x => x.time).FirstOrDefault();
+            if (min != null)
+            {
+                Debug.Log("Xóa 1 dòng");
+                HightScoreData.Remove(min);
+                if (timeFinish > min.time)
+                {
+                    HightScoreData = HightScoreData.OrderBy(x => x.time).ToList();
+                    for (int i = 0; i <= HightScoreData.Count; i++)
+                    {
+                        HightScoreData[i].rank = i + 1;
+                        GameObject lines = Instantiate(ScoreInfo, Column);
+                        lines.GetComponent<ScoreLine>().setText(HightScoreData[i]);
+                    }
+                }
+            }
+        }
+
     }
 }
+
 
 [System.Serializable]
 public class HighScoreContent
 {
-    public TextMeshProUGUI rank;
-    public TextMeshProUGUI name;
-    public TextMeshProUGUI score;
+    public int rank;
+    public string name;
+    public int time;
 }
