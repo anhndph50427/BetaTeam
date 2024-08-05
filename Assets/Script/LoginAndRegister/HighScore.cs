1920aa30a282678b32f13d0a12714b4ed99110d3
vinhlong264 <164327473+vinhlong264@users.noreply.github.com>
2024-08-05 11:06:17 +0700

    update

--- Assets/Script/LoginAndRegister/HighScore.cs
+++ Assets/Script/LoginAndRegister/HighScore.cs
@@ -1,6 +1,8 @@
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.Networking;
 
 
 public class HighScore : MonoBehaviour
@@ -12,7 +14,7 @@ public class HighScore : MonoBehaviour
     void Start()
     {
         HighScoreContent content = new HighScoreContent();
-        content.name = "Latina";
+        content.name = "001";
         content.time = timeFinish;
 
         foreach (Transform t in Column)
@@ -56,6 +58,76 @@ public class HighScore : MonoBehaviour
         }
 
     }
+
+    #region test
+    //IEnumerator GetScore()
+    //{
+    //    WWWForm getScore = new WWWForm();
+    //    getScore.AddField("token", PlayerPrefs.GetString("token"));
+    //    UnityWebRequest www = UnityWebRequest.Post("https://fpl.expvn.com/GetHighscore.php", getScore);
+    //    yield return www.SendWebRequest();
+    //    if (!www.isDone)
+    //    {
+    //        Debug.Log("ErrorGetScore");
+    //    }
+    //    else
+    //    {
+    //        Debug.Log("Finish");
+    //        string[] get = www.downloadHandler.text.Split('\n');
+    //        HighScoreContent content = new HighScoreContent();
+    //        for (int i = 1; i < get.Length; i++)
+    //        {
+    //            string[] cols = get[i].Split('\t');
+    //            Debug.Log("Name: " + cols[0]);
+    //            Debug.Log("Score: " + cols[1]);
+
+    //        }
+
+    //        //HighScoreContent content = new HighScoreContent();
+
+
+    //        foreach (Transform t in Column)
+    //        {
+    //            Destroy(t.gameObject);
+    //        }
+
+    //        if (HightScoreData.Count < 3)
+    //        {
+    //            Debug.Log("Thêm m?t dòng m?i");
+    //            HightScoreData.Add(content);
+
+    //            HightScoreData = HightScoreData.OrderBy(x => x.time).ToList();
+
+    //            for (int i = 0; i < HightScoreData.Count; i++)
+    //            {
+    //                HightScoreData[i].rank = i + 1;
+    //                GameObject lines = Instantiate(ScoreInfo, Column);
+    //                lines.GetComponent<ScoreLine>().setText(HightScoreData[i]);
+    //            }
+
+    //        }
+    //        else
+    //        {
+    //            var min = HightScoreData.OrderByDescending(x => x.time).FirstOrDefault();
+    //            if (min != null)
+    //            {
+    //                Debug.Log("Xóa 1 dòng");
+    //                HightScoreData.Remove(min);
+    //                if (timeFinish > min.time)
+    //                {
+    //                    HightScoreData = HightScoreData.OrderBy(x => x.time).ToList();
+    //                    for (int i = 0; i <= HightScoreData.Count; i++)
+    //                    {
+    //                        HightScoreData[i].rank = i + 1;
+    //                        GameObject lines = Instantiate(ScoreInfo, Column);
+    //                        lines.GetComponent<ScoreLine>().setText(HightScoreData[i]);
+    //                    }
+    //                }
+    //            }
+    //        }
+    //    }
+    //}
+    #endregion
 }
 
 
